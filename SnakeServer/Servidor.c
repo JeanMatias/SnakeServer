08868d6a839cc6618dc741da808c85de9899e55c
Jean Matias <jcsmatias84@gmail.com>
2017-05-17 19:59:30 +0100

    bump

--- SnakeServer/Servidor.c
+++ SnakeServer/Servidor.c
@@ -4,7 +4,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h> 
-#include "..\..\DLL\DLL\DLL.h"
+#include "..\..\SnakeDLL\\SnakeDLL\SnakeDLL.h"
 
 /* ----------------------------------------------------- */
 /*  VARIÁVEIS GLOBAIS									 */
